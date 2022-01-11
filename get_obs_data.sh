4008dce49fad006ec368141927b77de5b65e3003
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2022-01-11 10:31:40 -0800

    Started obs data download script

---
+++ get_obs_data.sh
@@ -0,0 +1,3 @@
+echo "Downloading ASCOS data"
+
+curl "ftp://ftp1.esrl.noaa.gov/psd3/cruises/ASCOS_2008/ODEN/radar/mmcr/DAMOCLES/ascos_mmcrmerge.C1.c1.20080831.000000.nc" -o "data/obs/ASCOS/ascos_mmcrmerge.C1.c1.20080831.000000.nc"
