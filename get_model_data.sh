2ec60b3211519cb81051517771070e1c9fecc0d9
Lucas Sterzinger <lsterzinger@ucdavis.edu>
2022-04-28 13:21:17 -0700

    Changes from review (#2)
    * Update gitignore
    
    * Updated case overview scripts
    
    * updates
    
    * Updated plotting scripts
    
    * Get and use CIN0 simulation data
    
    * Updated labels
    
    * Changed NoINP to IceNucOff
    
    * changed folder to figure-notebooks

--- get_model_data.sh
+++ get_model_data.sh
@@ -5,6 +5,11 @@ echo "Downloading RAMS model data from ${BASE_URL}"
 curl -C - "${BASE_URL}/ascos.nc" -o data/model/ascos.nc
 curl -C - "${BASE_URL}/oliktok.nc" -o data/model/oliktok.nc
 curl -C - "${BASE_URL}/summit.nc" -o data/model/summit.nc
+
+curl -C - "${BASE_URL}/ascos_cin0.nc" -o data/model/ascos_cin0.nc
+curl -C - "${BASE_URL}/oliktok_cin0.nc" -o data/model/oliktok_cin0.nc
+curl -C - "${BASE_URL}/summit_cin0.nc" -o data/model/summit_cin0.nc
+
 curl -C - "${BASE_URL}/oli_vert_momentum_flux.nc" -o data/model/oli_vert_momentum_flux.nc
 curl -C - "${BASE_URL}/ascos_vert_momentum_flux.nc" -o data/model/ascos_vert_momentum_flux.nc
 curl -C - "${BASE_URL}/smt_vert_momentum_flux.nc" -o data/model/smt_vert_momentum_flux.nc
